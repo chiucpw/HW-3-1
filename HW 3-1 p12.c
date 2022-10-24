ae7a9850a55b6d528b907366ba267114243b3182
chiucpw <chiujack5354@gmail.com>
2022-10-24 21:44:36 +0800

    HW 3-1

---
+++ HW 3-1 p12.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int square(int y);
+
+int main()
+{
+	int x;
+
+	for (x = 1; x <= 10; x++)
+	{
+		printf("%d ",square(x));
+	}
+	printf("\n");
+}
+
+int square(int y)
+{
+	return y * y;
+}
